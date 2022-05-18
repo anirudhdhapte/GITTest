fff340e2ee4e6d80943b29799e7c510ab16aa83f
ANI <ANI@DESKTOP-QTMEIJP>
2022-05-18 10:51:51 +0530

    1

--- GITTestUser1/src/gt/test/GITTestUser1.java
+++ GITTestUser1/src/gt/test/GITTestUser1.java
@@ -1,7 +1,7 @@
 package gt.test;
 
 public class GITTestUser1 {
-private static int x=20,y=20;
+private static int x=10,y=10;
 	public static void main(String[] args) {
 	System.out.println(x+y);
 }
