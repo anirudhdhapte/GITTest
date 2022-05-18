db83bb4ca85683c8b1ca47fcc3c27126a168c471
anirudhdhapte <75411928+anirudhdhapte@users.noreply.github.com>
2022-05-18 15:00:11 +0530

    update2

--- GITTestUser1/src/gt/test/GITTestUser1.java
+++ GITTestUser1/src/gt/test/GITTestUser1.java
@@ -1,7 +1,7 @@
 package gt.test;
 
 public class GITTestUser1 {
-private static int x=10,y=10,z=10;
+private static int x=10,y=10,z=10,a=10;
 	public static void main(String[] args) {
 	System.out.println(x+y);
 }
