e7ee0f3074f44a07b1491ac71b5b2a438dbc22d2
ANI <ANI@DESKTOP-QTMEIJP>
2022-05-23 11:42:01 +0530

    commit 11:41AM

--- GITTestUser1/src/gt/test/GITTestUser1.java
+++ GITTestUser1/src/gt/test/GITTestUser1.java
@@ -1,8 +1,8 @@
 package gt.test;
 
 public class GITTestUser1 {
-private static int x=10,y=10,z=10,a=10;
+private static int x=10,y=10;
 	public static void main(String[] args) {
-	System.out.println(x*y);
+	System.out.println(x+y);
 }
 }
