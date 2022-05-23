53b33772c5001e3fb2ed1151c6f399a6163ed060
ANI <ANI@DESKTOP-QTMEIJP>
2022-05-23 13:42:16 +0530

    ews2 v1.0

--- GITTestUser1/src/gt/test/GITTestUser1.java
+++ GITTestUser1/src/gt/test/GITTestUser1.java
@@ -3,6 +3,7 @@ package gt.test;
 public class GITTestUser1 {
 private static int x=10,y=10;
 	public static void main(String[] args) {
-	System.out.println(x+y);
+	System.out.println(x*y);
+	System.out.println("New Line");
 }
 }
